45d23669d6f5ff707b53b3dbdb3442e8e8a68b29
QuentinAbn <quentin.aubineau@epfedu.fr>
2023-12-13 14:53:54 +0100

    Lab step 3

--- services/epf-flower-data-science/main.py
+++ services/epf-flower-data-science/main.py
@@ -1,8 +1,13 @@
 import uvicorn
 
 from src.app import get_application
+from fastapi.responses import RedirectResponse
+
 
 app = get_application()
+@app.get("/")
+def redirect_to_swagger():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
